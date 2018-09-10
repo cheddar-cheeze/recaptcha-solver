8dbd42c5e471eed962b628eb2a6c571abd44a48c
cheddar-cheeze <cheddarcheeze@protonmail.com>
2018-09-09 20:02:06 -0500

    image scraper

--- image_scraper.py
+++ image_scraper.py
@@ -5,9 +5,9 @@ import hashlib
 import argparse
 
 parser = argparse.ArgumentParser(description="scrape the internet for training images")
-parser.add_argument('dir', metavar='Directory', type=str, nargs=1, help='Path to the testing data')
-parser.add_argument('label', metavar='Label', type=str, nargs=1, help='The name of the images to download')
-parser.add_argument('amt', metavar='Download Amount', type=int, nargs=1, help='The maximum amount of images to download')
+parser.add_argument('dir', type=str, nargs=1, help='Path to the testing data')
+parser.add_argument('label', type=str, nargs=1, help='The name of the images to download')
+parser.add_argument('amt', type=int, nargs=1, help='The maximum amount of images to download')
 args = parser.parse_args()
 directory = vars(args)['dir'][0]
 label = vars(args)['label'][0]
@@ -22,11 +22,11 @@ google_crawler.crawl(keyword=label, offset=0, filters={'type': 'photo', 'date': 
 print('Removing non valid image files')
 for file in os.listdir(dir):
     file_type = magic.from_file(os.path.join(dir, file), mime=True)
-    if (file_type != 'image/jpeg') or (file_type != 'image/png'):
-        os.remove(os.path.join(dir, file))
-        print(file + " was removed, is not a valid file format")
-    else:
+    if (file_type == 'image/jpeg') or (file_type == 'image/png'):
         with open(os.path.join(dir, file), 'rb') as f:
             shasum = hashlib.md5(f.read()).hexdigest()
             f.close()
-            os.rename(os.path.join(dir, file), os.path.join(dir, shasum + '.jpg'))
+            os.rename(os.path.join(dir, file), os.path.join(dir, shasum + '.{}'.format(file.split('.')[1])))
+    else:
+        os.remove(os.path.join(dir, file))
+        print(file + " was removed, is not a valid file format")
